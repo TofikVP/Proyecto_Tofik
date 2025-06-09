413202eb321131de5393ac36fa6aaa060cc4f807
TofikVP <tofik.varo@goumh.umh.es>
2025-06-09 14:00:59 +0200

    Correcion staticos 1

--- vandalproy/settings.py
+++ vandalproy/settings.py
@@ -123,9 +123,9 @@ LOCALE_PATHS = [
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "static/uploads"
