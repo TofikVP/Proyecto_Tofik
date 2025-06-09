037954db3b85b36ce76c19d7bb1c7f2df7671401
TofikVP <tofik.varo@goumh.umh.es>
2025-06-09 13:46:59 +0200

    Restablecer settings.py

--- vandalproy/settings.py
+++ vandalproy/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,17 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-o+lj84mim5&*!bu_3+(@j84om1pej+!=pq5_7z3kpb%8)8l4t-"
-SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-o+lj84mim5&*!bu_3+(@j84om1pej+!=pq5_7z3kpb%8)8l4t-")
+SECRET_KEY = "django-insecure-o+lj84mim5&*!bu_3+(@j84om1pej+!=pq5_7z3kpb%8)8l4t-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
 
-ALLOWED_HOSTS = [
-    ".onrender.com", "localhost", "127.0.0.1",
-    "195.55.200.162", "172.16.2.8", "vandal.ddaw.es", "www.vandal.ddaw.es"
-]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "195.55.200.162", "172.16.2.8", "proyecto-tofik.onrender.com"]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -80,19 +75,13 @@ WSGI_APPLICATION = "vandalproy.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -133,11 +122,9 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-# STATIC_URL = "/static/"
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "static/uploads"
