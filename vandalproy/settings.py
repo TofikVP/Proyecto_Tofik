45259b4ffb40de9b9fd6dbc23e88c779148dc55d
TofikVP <tofik.varo@goumh.umh.es>
2025-06-09 23:38:30 +0200

    Retoques finales 5

--- vandalproy/settings.py
+++ vandalproy/settings.py
@@ -124,9 +124,10 @@ LOCALE_PATHS = [
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+else:
+    STATICFILES_DIRS = []
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
